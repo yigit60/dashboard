f6eeb174d01d836be26a8579e6df551e2aa0d21e
Glitch (zula-botu) <none>
2018-10-14 09:55:25 +0000

    🍉🎀 Updated with Glitch

--- bot.js
+++ bot.js
@@ -1,14 +1,4 @@
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping tamamdır.");
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
+
 /////////////////////////////////////////////////////////////////////////////
 const Discord = require('discord.js');
 const client = new Discord.Client();
